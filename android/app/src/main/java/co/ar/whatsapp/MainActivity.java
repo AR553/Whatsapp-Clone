5d4080a8be048f9465bf637fa65b85cccc025cb9
abeer <abeerhussain99@gmail.com>
2020-10-05 13:08:47 +0500

    first commit

---
+++ android/app/src/main/java/co/ar/whatsapp/MainActivity.java
@@ -0,0 +1,6 @@
+package co.ar.whatsapp;
+
+import io.flutter.embedding.android.FlutterActivity;
+
+public class MainActivity extends FlutterActivity {
+}
---
+++ lib/constants.dart
@@ -0,0 +1,4 @@
+import 'package:flutter/material.dart';
+
+var kViewControllerTextStyle = TextStyle(
+    color: Color.fromRGBO(250, 250, 250, 100), fontWeight: FontWeight.bold);
